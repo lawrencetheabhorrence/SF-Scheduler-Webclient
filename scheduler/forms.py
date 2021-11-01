b5ab3a90c4a5043f6927dbdb979139d2af6de478
lawrencetheabhorrence <luluemilianolawrence@gmail.com>
2021-11-01 10:38:18 +0800

    hide secret key

--- scheduler/forms.py
+++ scheduler/forms.py
@@ -5,8 +5,5 @@ class Sportsfest(forms.Form):
     timeslots = forms.IntegerField(label='No. of Timeslots per Day', min_value=1)
     timeslot_length = forms.IntegerField(label='Timeslot length in minutes', min_value=1)
 
-class Game(forms.Form):
-    name = forms.CharField(label='Name of game', max_length=300)
-    n_timeslots = forms.IntegerField(label='Number of Timeslots per Round', min_value=1)
-    priority = forms.Select(choices=[(a,a) for a in ['Major','Minor','Junior']])
-    categories = forms.IntegerField('Number of Categories', min_value=1)
+class Game(forms.ModelForm):
+
