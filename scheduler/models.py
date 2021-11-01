6f2bad43b49fb04148d095b8ada6b26cf3d0caeb
lawrencetheabhorrence <luluemilianolawrence@gmail.com>
2021-11-01 10:12:42 +0800

    added Game model

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    name=models.CharField(max_length=300)
+    n_timeslots_per_round=models.IntegerField()
+    priority_choices = [(a,a) for a in ['Major','Minor','Junior']]
+    priority=models.CharField(choices=priority_choices,default='Minor')
